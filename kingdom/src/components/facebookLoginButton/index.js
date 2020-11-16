d0c10b1182a9e331b0530731a436342361231a06
DieterDoman <dieter.doman@takealot.com>
2020-11-16 13:25:37 +0200

    Improved login button.

--- kingdom/src/components/facebookLoginButton/index.js
+++ kingdom/src/components/facebookLoginButton/index.js
@@ -5,13 +5,20 @@ import {backend} from '../../config/backendServer';
 import axios from 'axios';
 
 export default class FacebookLoginButton extends Component {
+  constructor() {
+    super();
+    this.state = {
+      loggedIn: false,
+    };
+  }
   componentDidMount() {
     this.getAccessToken();
   }
 
   getAccessToken = () => {
     AccessToken.getCurrentAccessToken().then((data) => {
-      if (data.accessToken && data.userID) {
+      if (data && data.accessToken && data.userID) {
+        this.setState({loggedIn: true});
         axios
           .post(
             backend.url + '/facebook_login',
@@ -39,11 +46,20 @@ export default class FacebookLoginButton extends Component {
     );
   };
 
+  logout = () => {
+    LoginManager.logOut();
+    this.setState({loggedIn: false});
+  };
+
   render() {
+    const {loggedIn} = this.state;
+    const buttonText = loggedIn === true ? 'Logout' : 'Login';
+    const onPressFunction =
+      loggedIn === true ? this.logout : this.getAndSendFacebookAccessToken;
     return (
       <View>
-        <Button onPress={this.getAndSendFacebookAccessToken} title="Login">
-          Login
+        <Button onPress={onPressFunction} title={buttonText}>
+          {buttonText}
         </Button>
       </View>
     );
